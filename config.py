3653d824ebbbee563d2780b6dd086a766b9a0448
zhouyusong <zhouyusong@foxmail.com>
2017-01-13 17:09:19 +0800

    modify README.md and config,py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 DEBUG = True
 SECRET_KEY = 'this is built in depcare'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-UPLOAD_PATH = '/home/deepcare/repos/pic2pic/myapp/static/upload_images'
+UPLOAD_PATH = '/home/deepcare/repos/image_retrival/myapp/static/upload_images'
 MYSQL_DATABASE_USER = 'root'
 MYSQL_DATABASE_PASSWORD = '123456'
 MYSQL_DATABASE_DB = 'tumor'
